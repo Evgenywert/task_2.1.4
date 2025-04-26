c9d657ab337cbc224a1ec8bd58d92040d207ed47
Евгений Капустянский <evgenijkapustanskij@Evgenij-MacBook-Pro.local>
2025-04-26 21:14:32 +0300

    author

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,16 @@
 package koschei.models;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class Needle7 {
+    private final Deth8 deth;
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth.toString();
     }
 }
--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -1,20 +1,19 @@
 package koschei.models;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class Wood3 {
 
     private final Rabbit4 rabbit;
 
-    @Autowired
-    public Wood3(Rabbit4 rabbit) {
-        this.rabbit = rabbit;
-    }
 
     @Override
     public String toString() {
+
         return ", на дереве заяц " + rabbit.toString();
     }
 }
